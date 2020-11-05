61bbecce47751c0eee8d187519baadc43859b666
Jose Miguel Cabrera <cabreraj525@gmail.com>
2020-11-05 02:06:34 -0600

    fix create users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'rol'=>['required','integer'],
         ]);
     }
 
@@ -69,7 +68,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'roluser' =>$data['rol'],
         ]);
         // Le asignamos el rol de Cliente
         //$user->assignRole('admin');
